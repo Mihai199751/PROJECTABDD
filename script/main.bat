8b289ec9d0f39911b7bccc345c7ff65b3b9ecdbc
EL MEZOURI KARIM <KARIM.ELMZR@ge-pedago.etat-ge.ch>
2024-12-20 14:14:50 +0100

    main.bat script with echo

---
+++ script/main.bat
@@ -0,0 +1,13 @@
+@ECHO OFF
+CLS
+:MENU
+CLS
+ECHO.
+ECHO ..........................................................
+ECHO Choissiez 1 pour DROP | 2 pour CREATE | 3 pour --Quitter--
+ECHO ..........................................................
+ECHO.
+ECHO 1 - DROP ALL 
+ECHO 2 - CREATE ALL
+ECHO 3 - QUITTER
+ECHO.
